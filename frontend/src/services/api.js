aa221678f3d9d8577689743e1c1f29f31047c17e
jaleel <syedjaleel01071995@gmail.com>
2026-02-06 11:35:18 +0530

    Update API URL to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/v1';
+const API_URL = 'https://anythingaibackend.onrender.com/api/v1';
 
 const getToken = () => localStorage.getItem('token');
 
